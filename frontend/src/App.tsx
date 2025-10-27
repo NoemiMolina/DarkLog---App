4b3813c99ec840f329921213ac02edf7da7b3cac
NoemiMolina <molinanoemi5@gmail.com>
2025-10-27 16:18:11 +0100

    the fight with tailwind was hard, time to dl shadcn now

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-red-500 flex items-center justify-center text-3xl">
+      Hell yeah it works ! 
+    </div>
+  );
+}
+
+export default App;
