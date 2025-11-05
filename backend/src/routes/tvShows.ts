0315f3a46dc9ef6b3aa9e6b2e3cc2538d1f74bf2
NoemiMolina <molinanoemi5@gmail.com>
2025-11-05 14:56:36 +0100

    order of routes modified so tvshow random and search can work

--- backend/src/routes/tvShows.ts
+++ backend/src/routes/tvShows.ts
@@ -12,11 +12,12 @@ import {
 const router = express.Router();
 
 router.get("/", getAllTVShows);
+router.get("/random", getRandomTVShow);
+router.get("/search", searchTVShow);
 router.get("/:tvShowId", getTVShowById);
 router.get("/:tvShowId/seasons/:seasonNumber", getSeasonByNumber);
 router.get("/:tvShowId/seasons/:seasonNumber/episodes/:episodeNumber", getEpisodeByNumber);
 router.post("/:tvShowId/seasons/:seasonNumber/episodes/:episodeNumber/rating", addRatingToEpisode);
-router.get("/random", getRandomTVShow);
-router.get("/search", searchTVShow);
+
 
 export default router;
