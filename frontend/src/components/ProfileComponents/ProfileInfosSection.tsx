266054f3200d48bb33c174d343402a980d2af7bf
Darkno <darkno@mbp-de-fr.home>
2026-02-03 14:43:37 +0100

    news pics added

--- frontend/src/components/ProfileComponents/ProfileInfosSection.tsx
+++ frontend/src/components/ProfileComponents/ProfileInfosSection.tsx
@@ -4,7 +4,6 @@ import { Button } from '../../components/ui/button';
 import { Input } from '../../components/ui/input';
 import { Label } from '../../components/ui/label';
 import { Eye, EyeOff, Save, Edit, X, Camera } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
 
 interface ProfileInfoSectionProps {
   profileData: {
